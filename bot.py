aad53246478ac689b6e72121299f4719b4535e66
FUNCER <118087385+Sudox00@users.noreply.github.com>
2023-06-19 23:31:03 +0300

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,23 +1,25 @@
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from time import sleep
-
+from random import randint
 try:
-	f = open('token.txt')
+	z = open('token.txt')
 except:	
 	my_file = open("token.txt", "w")
 	my_file.close()
-	f = open('token.txt')
 	
-if f.read() == "":
+if z.read() == "":
 	access_token = input("Введите ваш access_token: ")
 	f = open('token.txt', 'w')
 	f.write(str(access_token))
 	
 else:
+	f = open('token.txt')
 	access_token=str(f.read())
+	
 
-session = vk_api.VkApi(token=access_token)
+f = open('token.txt')	
+session = vk_api.VkApi(token=str(access_token))
 vk=session.get_api()
 longpoll = VkLongPoll(session)
 
@@ -30,7 +32,7 @@ ids = cc.split(",")
 msg = input("Введтие сообщение: ")
 
 timeout = input("Задержка(в сек.): ")
-exit()
+
 while True:
 	for x in ids:
 		try:
